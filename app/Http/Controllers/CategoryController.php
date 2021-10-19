1700ae66dd2a8b049903f19741a1cc7d58ab5806
rewe999 <zymek998@gmail.com>
2021-10-19 13:12:15 +0200

    #7 - Add Controller Logic

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
-    //
+    public function manageCategory($id=0)
+    {
+        $categories = Category::where('parent_id', '=',$id)->get();
+        $allCategories = Category::where('parent_id', '=',$id)->get();
+        $name = Category::where('id',$id)->first();
+        return view('category.CategoryView',compact('categories','allCategories','id','name'));
+    }
+
+    public function addCategory(Request $request, $id = 0)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+        ]);
+        $input = $request->all();
+        $input['parent_id'] = empty($input['parent_id']) ? $id : $input['parent_id'];
+
+        Category::create($input);
+        return back()->with('success', 'New Category added successfully.');
+    }
+
+    public function editCategory($id){
+        $category = Category::findOrFail($id);
+        $allCategories = Category::where('parent_id', '<',$id)->get();
+        return view('category.CategoryEdit',compact(['id','allCategories','category']));
+    }
+
+    public function updateCategory($id,Request $request){
+        $category = Category::findOrFail($id);
+        $category->title = $request['title'];
+        $category->parent_id = $request['parent_id'];
+        $category->save();
+
+        return back()->with('success', 'Category has been edit');
+    }
+
+    public function removeCategory($id){
+        DB::table('categories')->where('parent_id',$id)->delete();
+        Category::findOrFail($id)->delete();
+        return redirect()->route('get.category')->with('success', 'Category has been deleted');
+    }
 }
