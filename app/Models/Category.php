7d75a9fbb8cf9c958d74e333b6aa5a81dcc193e4
rewe999 <zymek998@gmail.com>
2021-10-19 12:53:55 +0200

    #2 - change model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public $fillable = ['title','parent_id'];
+
+    public function childs() {
+        return $this->hasMany(Category::class,'parent_id','id') ;
+    }
 }
