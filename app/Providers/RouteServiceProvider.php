5857a49b07d6885fc77f070170dbaec50e982baf
rewe999 <zymek998@gmail.com>
2021-10-19 13:24:45 +0200

    #10 - redirect after login/logout

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const CATEGOREIS = '/category';
 
     /**
      * The controller namespace for the application.
